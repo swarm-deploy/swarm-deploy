22f94e1b8e614d1e8064dec9f5cd2e2c06d2512d
artarts36 <temicska99@mail.ru>
2026-05-24 00:39:55 +0300

    feat: add service deploy

--- internal/compose/service.go
+++ internal/compose/service.go
@@ -9,13 +9,14 @@ import (
 type Services []Service
 
 type Service struct {
-	Name        string      `yaml:"name" json:"name"`
-	Image       string      `yaml:"image" json:"image"`
-	Environment Environment `yaml:"environment" json:"environment,omitempty"`
-	Networks    []string    `yaml:"networks" json:"networks,omitempty"`
-	Secrets     []ObjectRef `yaml:"secrets" json:"secrets,omitempty"`
-	Configs     []ObjectRef `yaml:"configs" json:"configs,omitempty"`
-	InitJobs    []InitJob   `yaml:"x-init-deploy-jobs" json:"init_jobs,omitempty"`
+	Name        string        `yaml:"name" json:"name"`
+	Image       string        `yaml:"image" json:"image"`
+	Environment Environment   `yaml:"environment" json:"environment,omitempty"`
+	Networks    []string      `yaml:"networks" json:"networks,omitempty"`
+	Secrets     []ObjectRef   `yaml:"secrets" json:"secrets,omitempty"`
+	Configs     []ObjectRef   `yaml:"configs" json:"configs,omitempty"`
+	InitJobs    []InitJob     `yaml:"x-init-deploy-jobs" json:"init_jobs,omitempty"`
+	Deploy      ServiceDeploy `yaml:"deploy" json:"deploy"`
 }
 
 func (s *Services) UnmarshalYAML(n *yaml.Node) error {
