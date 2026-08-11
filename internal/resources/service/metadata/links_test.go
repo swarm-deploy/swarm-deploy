796102d37067c6c91cffb4d5a0aef441608b0d0e
Artem Ukrainskii <temicska99@mail.ru>
2026-08-12 00:24:36 +0300

    remove Traefik OpenAPI label tests

--- internal/resources/service/metadata/links_test.go
+++ internal/resources/service/metadata/links_test.go
@@ -10,45 +10,15 @@ import (
 func TestLinksResolverResolveOpenAPI(t *testing.T) {
 	resolver := NewLinksResolver()
 
-	cases := []struct {
-		name     string
-		labels   Labels
-		expected []Link
-	}{
-		{
-			name: "swarm deploy label",
-			labels: Labels{
-				Service: map[string]string{
-					labelsdict.OpenAPIURL: "https://api.example.com/openapi.json",
-				},
-			},
-			expected: []Link{{Type: "OpenAPI", URL: "https://api.example.com/openapi.json"}},
+	meta := &Metadata{}
+	resolver.Resolve(Labels{
+		Service: map[string]string{
+			labelsdict.OpenAPIURL: "https://api.example.com/openapi.json",
 		},
-		{
-			name: "traefik api portal label",
-			labels: Labels{
-				Service: map[string]string{
-					"traefik.http.services.myapi-svc.loadbalancer.apiportal.path": "/openapi.json",
-				},
-			},
-			expected: []Link{{Type: "OpenAPI", URL: "/openapi.json"}},
-		},
-		{
-			name: "traefik api portal label from container",
-			labels: Labels{
-				Container: map[string]string{
-					"traefik.http.services.myapi-svc.loadbalancer.apiportal.path": "/swagger.json",
-				},
-			},
-			expected: []Link{{Type: "OpenAPI", URL: "/swagger.json"}},
-		},
-	}
+	}, meta)
 
-	for _, tc := range cases {
-		t.Run(tc.name, func(t *testing.T) {
-			meta := &Metadata{}
-			resolver.Resolve(tc.labels, meta)
-			assert.ElementsMatch(t, tc.expected, meta.Links)
-		})
-	}
+	assert.Equal(t, []Link{{
+		Type: "OpenAPI",
+		URL:  "https://api.example.com/openapi.json",
+	}}, meta.Links)
 }
