ea7a12a29a70afc4bfcbc1862de3468616b7f257
artarts36 <temicska99@mail.ru>
2026-09-14 12:07:37 +0300

    feat: extract parent span

---
+++ internal/entrypoints/webserver/middlewares/trace.go
@@ -0,0 +1,19 @@
+package middlewares
+
+import (
+	"net/http"
+
+	"go.opentelemetry.io/otel"
+	"go.opentelemetry.io/otel/propagation"
+)
+
+func Trace(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		ctx := otel.GetTextMapPropagator().Extract(
+			r.Context(),
+			propagation.HeaderCarrier(r.Header),
+		)
+
+		next.ServeHTTP(w, r.WithContext(ctx))
+	})
+}
